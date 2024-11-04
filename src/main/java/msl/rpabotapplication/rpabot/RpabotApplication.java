4b420117d384c308b6051106ccc4ff5ba2fe7fa6
Sakshi Bhatt <30sakshibhatt@gmail.com>
2024-11-04 18:34:46 +0530

    Project Initial Setup

---
+++ src/main/java/msl/rpabotapplication/rpabot/RpabotApplication.java
@@ -0,0 +1,13 @@
+package msl.rpabotapplication.rpabot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RpabotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpabotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/msl/rpabotapplication/rpabot/RpabotApplicationTests.java
@@ -0,0 +1,13 @@
+package msl.rpabotapplication.rpabot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpabotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
